7ee723a975fe2155e4d0e257410202ccf15a06f4
james <james@LAPTOP-CP8LTSAN>
2021-04-30 17:18:02 -0400

    Added a comment

--- src/seleniumsessions/WebDriverBasics.java
+++ src/seleniumsessions/WebDriverBasics.java
@@ -7,9 +7,14 @@ public class WebDriverBasics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		// for Mozilla Fire fox
+
 		// System.setProperty("webdriver.gecko.driver","//C:\\Users\\james\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
 		// WebDriver driver = new FirefoxDriver();
 		// driver.get("http://www.google.com"); //launching google
+
+		// for Google Chrome
 
 		System.setProperty("webdriver.chrome.driver",
 				"//C:\\Users\\james\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
@@ -18,13 +23,15 @@ public class WebDriverBasics {
 		String title = driver.getTitle();
 		System.out.println(title);
 
+		// expected =actual outcome
+		// Validation point for testing
 		if (title.equals("Google")) {
 			System.out.println("Correct title");
 		} else {
 			System.out.println("Incorrect Title");
 		}
-		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getPageSource());
+		System.out.println(driver.getCurrentUrl()); // for current url
+		System.out.println(driver.getPageSource()); // for page source
 		driver.quit();
 	}
 
